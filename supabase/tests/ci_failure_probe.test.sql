3b7c230a1bc8511dda64ce27882ea212033cabf8
Lorenzo Roque Dal Fabbro <theneolorenzo@gmail.com>
2026-09-08 18:37:34 +0100

    test: remove pgTAP CI failure probe

--- supabase/tests/ci_failure_probe.test.sql
+++
@@ -1,8 +0,0 @@
-begin;
-
-create extension if not exists pgtap with schema extensions;
-select plan(1);
-select ok(false, 'intentional pgTAP validation probe for issue #34');
-select * from finish();
-
-rollback;
